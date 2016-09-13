004909d7885e1bf8f83c1cd9d45318a3c78c27b3
Jean Joeris <joeris.peter@gmail.com>
2016-09-13 07:52:06 -0600

    update item to have an optional longer description, and add model test to verify this

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160913023501) do
+ActiveRecord::Schema.define(version: 20160913135045) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,8 +20,9 @@ ActiveRecord::Schema.define(version: 20160913023501) do
     t.string   "description"
     t.integer  "price"
     t.string   "image_path"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+    t.text     "long_description"
   end
 
 end
