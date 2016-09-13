ac93086ec4a2203299e79941741c0a608f9566b7
Jean Joeris <joeris.peter@gmail.com>
2016-09-12 19:00:57 -0600

    add feature test for user seeing items, along with test gems

---
+++ spec/features/user_sees_items.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.feature "User sees all the items" do
+  scenario "User visits items page" do
+    # Given items exist
+    # As a User
+    # when I visit the items page
+    # Then I expect to see all the items
+  end
+end
