a5ba1ee98e16cc449bb40642564c0958d7026a8f
Jean Joeris <joeris.peter@gmail.com>
2016-09-13 17:34:36 -0600

    add orders migration and model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160913193416) do
+ActiveRecord::Schema.define(version: 20160913233122) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,12 @@ ActiveRecord::Schema.define(version: 20160913193416) do
     t.text     "long_description"
     t.integer  "category_id"
     t.index ["category_id"], name: "index_items_on_category_id", using: :btree
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   add_foreign_key "items", "categories"
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,12 +1,13 @@
 require "rails_helper"
 
-RSpec.decribe Order, type: :model do
-  it { should have_many :ordered_items }
-  it { should have_many :items }
-
+RSpec.describe Order, type: :model do
   it "has user ID" do
     order = Order.new(user_id: 1)
 
     expect(order.user_id).to eq(1)
   end
+  
+  it { should have_many :ordered_items }
+  it { should have_many :items }
+
 end
