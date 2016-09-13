1d2c983b11fcb57932ba826e8a5714c3b2a70ebe
Jean Joeris <joeris.peter@gmail.com>
2016-09-12 20:44:27 -0600

    add items index view and fixed test typo

--- spec/features/user_sees_items_spec.rb
+++ spec/features/user_sees_items_spec.rb
@@ -1,15 +1,33 @@
 require 'rails_helper'
 
 RSpec.feature "User sees all the items" do
-  scenario "User visits items page" do
-    # Given items exist
-    # As a User
-    # when I visit the items page
-    # Then I expect to see all the items
-    Item.create(name: "Yak", quantity: "200", price: "150")
+  context "when items already exist" do
+    scenario "User visits items page with one item" do
+      # Given an item exists
+      # As a User
+      # when I visit the items page
+      # Then I expect to see the item
+      Item.create(name: "Yak", quantity: "200", price: "150")
 
-    visit items_index
+      visit items_path
 
-  expect(page).to have_content "Yak"
+      expect(page).to have_content "Yak"
+    end
+
+    scenario "User visits items page with many items" do
+      # Given items exist
+      # As a User
+      # when I visit the items page
+      # Then I expect to see all the items
+      Item.create(name: "Yak", quantity: "200", price: "150")
+      Item.create(name: "Silk Yarn", quantity: "50", price: "23")
+      Item.create(name: "Yo-Yo", quantity: "2000", price: "5")
+
+      visit items_path
+
+      expect(page).to have_content "Yak"
+      expect(page).to have_content "Silk Yarn"
+      expect(page).to have_content "Yo-Yo"
+    end
   end
 end
