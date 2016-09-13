af5479d019d489b8f618027a7a161e52e1b9280e
Raphael Barbo <rsbarbo@gmail.com>
2016-09-13 14:20:34 -0600

    give space between comments pounds

--- spec/features/users_sees_categories_items_spec.rb
+++ spec/features/users_sees_categories_items_spec.rb
@@ -2,12 +2,12 @@ require "rails_helper"
 
 RSpec.feature "Users can see category items" do
   scenario "page displays items in category" do
-    #Given that category exist
-    #And items are assigned
-    #As a user
-    #I visit '/category_name'
-    #I see the name of the category
-    #I also see all the items that belongs to that category
+    # Given that category exist
+    # And items are assigned
+    # As a user
+    # I visit '/category_name'
+    # I see the name of the category
+    # I also see all the items that belongs to that category
     category = Category.create(name: "grocery")
     category.items.create(title: "Banana",
                           description: "Wholesome Yellow Goodness",
@@ -21,12 +21,12 @@ RSpec.feature "Users can see category items" do
   end
 
   scenario "page displays items in category" do
-    #Given that category exist
-    #And items are assigned
-    #As a user
-    #I visit '/category_name'
-    #I see the name of the category
-    #I also see all the items that belongs ONLY to that category
+    # Given that category exist
+    # And items are assigned
+    # As a user
+    # I visit '/category_name'
+    # I see the name of the category
+    # I also see all the items that belongs ONLY to that category
     category_1 = Category.create(name: "grocery")
     category_1.items.create(title: "Banana",
                             description: "Wholesome Yellow Goodness",
