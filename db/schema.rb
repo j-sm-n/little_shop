2e7bfff4e3018c01de4d6ae02193dc6d19451b92
Jasmin Hudacsek <jasmin@hudacsek.com>
2016-09-13 21:18:45 -0600

    Add user migration, model and controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160913193416) do
+ActiveRecord::Schema.define(version: 20160914030905) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,15 @@ ActiveRecord::Schema.define(version: 20160913193416) do
     t.text     "long_description"
     t.integer  "category_id"
     t.index ["category_id"], name: "index_items_on_category_id", using: :btree
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "password_digest"
+    t.string   "gender"
+    t.string   "user_image",      default: "https://s22.postimg.org/8r7f8ry81/user.png"
+    t.datetime "created_at",                                                             null: false
+    t.datetime "updated_at",                                                             null: false
   end
 
   add_foreign_key "items", "categories"
