94914e9c5b95c937af1188810b1e00432f9a3019
Jasmin Hudacsek <jasmin@hudacsek.com>
2016-09-13 20:58:03 -0600

    Add login and dashboard routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   resources :items, only: [:index]
 
+  resources :users, only: [:new]
+
+  get '/login', to: 'sessions#new'
+
+  get '/dashboard', to: 'user#show'
+
   get "/cart", to: "cart#show"
   post "/cart", to: "cart#create"
 
