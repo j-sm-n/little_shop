1bb03a08ed510d65aa434fedf8ac38f91a58cc86
Jasmin Hudacsek <jasmin@hudacsek.com>
2016-09-13 20:59:22 -0600

    Add user creation feature test

---
+++ spec/features/user_creation_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.feature 'Visiter can create account', type: :feature do
+  scenario 'when visitor goes to login page' do
+    # As a visitor
+    # When I visit "/login"
+    # And when I click link "Create Account"
+    # And I fill in my desired credentials
+    # And I submit my information
+    # Then my current page should be "/dashboard"
+    # And I should see a message in the navbar that says "Logged in as SOME_USER"
+    # And I should see my profile information
+    # And I should not see a link for "Login"
+    # And I should see a link for "Logout"
+    visit login_path
+    click_on 'Create Account'
+
+    expect(current_path).to eq(new_user_path)
+
+    fill_in 'Username', with: 'becky_123'
+    fill_in 'Password', with: '12345'
+    fill_in 'Password Confirmation', with: '12345'
+    select('Female', from: 'Gender')
+    click_on "Register"
+
+    expect(current_path).to eq('/dashboard')
+    expect(page).to have_content 'Logged in as becky_123'
+    expect(page).to have_content 'Female'
+    expect(page).to_not find_link('Login').visible?
+    expect(page).to find_link('Logout').visible?
+  end
+end
