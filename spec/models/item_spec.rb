9953f2044872dbd0810d4aca13b465403008b582
Jean Joeris <joeris.peter@gmail.com>
2016-09-15 15:42:27 -0600

    Change item model test to use factory girl where possible

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -3,17 +3,10 @@ require 'rails_helper'
 RSpec.describe Item, type: :model do
   context 'with sufficient attributes' do
     it 'has the right attributes' do
-      item = Item.create(title: 'Dead Dove',
-                         description: "I don't know what I expected",
-                         long_description: 'Sometimes you have to return'\
-                         "a dead dove. You really don't want your brother, or"\
-                         'anyone else eating it. This is an Arrested '\
-                         'Development joke, in case you missed that',
-                         price: 8,
-                         image_path: 'i.ytimg.com/vi/nmLqs8qkP_Y/hqdefault.jpg')
+      item = create(:item_with_long_description)
 
       expect(item).to be_valid
-      expect(item.title).to eq('Dead Dove')
+      expect(item.title).to include('Dead Dove')
       expect(item.description).to eq("I don't know what I expected")
       expect(item.long_description).to include('Arrested Development')
       expect(item.price).to eq(8)
@@ -21,10 +14,7 @@ RSpec.describe Item, type: :model do
     end
 
     it "doesn't require long description" do
-      item = Item.create(title: 'Dead Dove',
-                         description: "I don't know what I expected",
-                         price: 8,
-                         image_path: 'i.ytimg.com/vi/nmLqs8qkP_Y/hqdefault.jpg')
+      item = create(:item)
       expect(item).to be_valid
     end
   end
--- spec/support/factories.rb
+++ spec/support/factories.rb
@@ -1,3 +1,16 @@
 FactoryGirl.define do
-  
+  factory :item do
+    sequence(:title) { |n| "Dead Dove #{n}" }
+    description "I don't know what I expected"
+    price 8
+    image_path "i.ytimg.com/vi/nmLqs8qkP_Y/hqdefault.jpg"
+
+    factory :item_with_long_description do
+      long_description "Sometimes you have to return"\
+      "a dead dove. You really don't want your brother, or"\
+      "anyone else eating it. This is an Arrested "\
+      "Development joke, in case you missed that"
+    end
+  end
+
 end
