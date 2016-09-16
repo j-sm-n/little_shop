a8a7dee29bf3c516dcf0b86d167c21056abe16c9
Jasmin Hudacsek <jasmin@hudacsek.com>
2016-09-16 10:48:39 -0600

    Add feature test for user security

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,6 @@ module ApplicationHelper
   def cents_to_dollar(cents)
     number_to_currency(cents / 100.0)
   end
+
+  def 
 end
---
+++ spec/features/user_authenticates_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.feature "User Authenticates", type: :feature do
+  scenario "user cannot view other users' current or past orders" do
+    # I cannot view another user's private data (current or past orders, etc)
+  end
+
+  scenario " " do
+    # I cannot view the administrator screens or use admin functionality
+  end
+
+  scenario " " do
+    # I cannot make myself an admin
+  end
+end
