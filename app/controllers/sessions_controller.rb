ae440c1032cf82c3edda36228f209288ef0a927b
Raphael Barbo <rsbarbo@gmail.com>
2016-09-15 19:24:13 -0600

    update session controller with parameters for admin vs user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,10 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(username: params[:session][:username])
-    if user && user.authenticate(params[:session][:password])
+    if user && user.authenticate(params[:password]) && user.is_admin?
+      session[:user_id] = user.id
+      redirect_to admin_dashboard_path
+    elsif user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
       redirect_to dashboard_path
     else
