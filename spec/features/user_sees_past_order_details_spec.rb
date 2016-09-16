498f3d3248689c98d926f7827b0845cd455cb031
Jean Joeris <joeris.peter@gmail.com>
2016-09-15 19:31:40 -0600

    Wrote test for user viewing past order

---
+++ spec/features/user_sees_past_order_details_spec.rb
@@ -0,0 +1,51 @@
+require "rails_helper"
+
+RSpec.feature "User sees past order" do
+  scenario "details on order page" do
+    # Given a user with orders
+    # As an authenticated user
+    # When I visit "/orders"
+    # Then I should see my past order
+    # And I should see a link to view that order
+    # And when I click that link
+    # Then I should see each item that was order with the quantity and line-item subtotals
+    # And I should see links to each item's show page
+    # And I should see the current status of the order **(ordered, paid, cancelled, completed)**
+    # And I should see the total price for the order
+    # And I should see the date/time that the order was submitted
+    # If the order was completed or cancelled
+    # Then I should see a timestamp when the action took place
+    user = create(:user)
+
+    order = user.orders.create(status: "paid")
+    order.items.create(title: "Banana", description: "Wholesome Yellow Goodness", price: 19, image_path: "placeholder")
+    order.items.create(title: "Banana", description: "Wholesome Yellow Goodness", price: 19, image_path: "placeholder")
+    order.items.create(title: "Apple", description: "Wholesome Red Goodness", price: 21, image_path: "placeholder")
+
+    visit root_path
+    click_link "Login"
+
+    fill_in "Username", with: user.username
+    fill_in "Password", with: user.password
+    click_on "login-button"
+
+    visit "/orders"
+
+    expect(page).to have_link("Order #1")
+
+    click_link("Order #1")
+
+    expect(page).to have_content("Quantity: 2")
+    expect(page).to have_content("Subtotal: $0.38")
+    expect(page).to have_content("Quantity: 1")
+    expect(page).to have_content("Subtotal: $0.21")
+
+    expect(page).to have_link("Banana")
+    expect(page).to have_link("Apple")
+
+    expect(page).to have_content("Status: paid")
+
+    expect(page).to have_content("Total: $0.59")
+
+  end
+end
