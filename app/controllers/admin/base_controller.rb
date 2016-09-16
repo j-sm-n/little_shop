6c8fc4703b4b0ef1c609195cd74b50600bd94df9
Raphael Barbo <rsbarbo@gmail.com>
2016-09-16 10:40:29 -0600

    Update base and users controller to add module

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,7 +1,9 @@
-class Admin::BaseController < ApplicationController
-  before_action :require_admin
+module Admin
+  class BaseController < ApplicationController
+    before_action :require_admin
 
-  def require_admin
-    render file: 'public/404.html' unless current_admin?
+    def require_admin
+      render file: 'public/404.html' unless current_admin?
+    end
   end
 end
