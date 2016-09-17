dad8147db7fc12bb2d2eb06ae6ac26d27995d951
Jasmin Hudacsek <jasmin@hudacsek.com>
2016-09-17 10:21:05 -0600

    Remove white space from application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,4 @@ module ApplicationHelper
   def cents_to_dollar(cents)
     number_to_currency(cents / 100.0)
   end
-
-  def 
 end
--- spec/features/user_authenticates_spec.rb
+++ spec/features/user_authenticates_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.feature "User Authenticates", type: :feature do
   scenario "user cannot view other users' current or past orders" do
     # I cannot view another user's private data (current or past orders, etc)
+    # user = User
   end
 
   scenario " " do
