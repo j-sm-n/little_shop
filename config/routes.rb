6178e77141bd171c93f2a0858b148c615dd4732c
Raphael Barbo <rsbarbo@gmail.com>
2016-09-17 12:14:40 -0600

    Create route for admin-users-edit

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
 
   namespace :admin do
     get 'dashboard', to: 'users#show', as: 'dashboard'
+    get 'dashboard/edit', to: 'users#edit', as: 'edit_dashboard'
   end
 
   get '/login', to: 'sessions#new'
