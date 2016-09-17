a04d77792ff18cc659808990b1da2e83c8c41137
Jean Joeris <joeris.peter@gmail.com>
2016-09-17 10:55:00 -0600

    Update category and category controller to use slug

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,14 @@
 class Category < ApplicationRecord
   has_many :items
 
+  validates :slug, presence: true
+
+  def initialize(*args)
+    super
+    update_attribute(:slug, name.downcase.gsub(" ", "-"))
+  end
+
   def to_param
-    name.tr(" ", "_")
+    slug
   end
 end
