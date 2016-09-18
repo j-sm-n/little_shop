cda73b7b24275288fd2ce121970b73c6530ecb99
Jean Joeris <joeris.peter@gmail.com>
2016-09-18 16:49:47 -0600

    Add method tests to order model test

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -25,18 +25,55 @@ RSpec.describe Order, type: :model do
     expect(order.total).to eq(99)
   end
 
-  it "has a status of 'Ordered'" do
+  it "has item quantity" do
     order = create(:order)
-    order.items.create(title: "banana",
+    banana = Item.create(title: "banana",
                        description: "Wholesome Yellow Goodness",
                        price: 19,
                        image_path: "placeholder-path")
 
-    order.items.create(title: "tea",
-                       description: "its tea...",
-                       price: 80,
+    3.times do
+      order.ordered_items.create(item: banana)
+    end
+
+    expect(order.quantity(banana.id)).to eq(3)
+  end
+
+  it "has item subtotal" do
+    order = create(:order)
+    banana = Item.create(title: "banana",
+                       description: "Wholesome Yellow Goodness",
+                       price: 19,
                        image_path: "placeholder-path")
 
-    expect(order.status).to eq("Ordered")
+    3.times do
+      order.ordered_items.create(item: banana)
+    end
+
+    expect(order.subtotal(banana.id)).to eq(3*banana.price)
+  end
+
+  context "statuses" do
+    it "has a status" do
+      order = create(:order)
+      order.items.create(title: "banana",
+                         description: "Wholesome Yellow Goodness",
+                         price: 19,
+                         image_path: "placeholder-path")
+
+      order.items.create(title: "tea",
+                         description: "its tea...",
+                         price: 80,
+                         image_path: "placeholder-path")
+
+      expect(order.status).to eq("Ordered")
+    end
+
+    it "can change status" do
+      order = create(:order)
+      order.change_status("amazing order")
+
+      expect(order.status).to eq("amazing order")
+    end
   end
 end
