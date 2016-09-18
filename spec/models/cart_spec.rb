7d696a39d39f83639fb27d89637d83962dcc2d21
Jean Joeris <joeris.peter@gmail.com>
2016-09-18 17:16:34 -0600

    Add method testing to cart model

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -27,4 +27,48 @@ RSpec.describe Cart, type: :model do
 
     expect(cart.contents).to eq({"53" => 3, "1" => 1})
   end
+
+  it "knows item quantities" do
+    cart = Cart.new
+
+    3.times do
+      cart.add_item("53")
+    end
+
+    expect(cart.quantity("53")).to eq(3)
+  end
+
+  it "finds item subtotal" do
+    cart = Cart.new
+    item = create(:item)
+
+    2.times do
+      cart.add_item(item.id)
+    end
+
+    expect(cart.item_subtotal(item.id)).to eq(2 * item.price)
+  end
+
+  it "finds cart total" do
+    cart = Cart.new
+    item = create(:item)
+    item_2 = create(:item)
+    item_2.update_attribute(:price, 200)
+
+    2.times do
+      cart.add_item(item.id)
+    end
+    cart.add_item(item_2.id)
+
+    expect(cart.total_price).to eq(item.price * 2  + item_2.price)
+  end
+
+  it "can clear contents" do
+    cart = Cart.new
+    cart.add_item("53")
+
+    cart.clear
+
+    expect(cart.contents).to eq({})
+  end
 end
