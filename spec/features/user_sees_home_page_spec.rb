cfd24d959e249a24047dcc699c3c346330f21e26
Raphael Barbo <rsbarbo@gmail.com>
2016-09-19 14:52:19 -0600

    Test pass for feature test home page

---
+++ spec/features/user_sees_home_page_spec.rb
@@ -0,0 +1,12 @@
+require "rails_helper"
+
+RSpec.feature "User can see home page" do
+  scenario "when user enter webpage address, he/she can see root page" do
+
+    visit "/"
+
+    expect(page).to have_content("HOME PAGE")
+    expect(page).to have_content("Login")
+    expect(page).to have_content("Shop")
+  end
+end
