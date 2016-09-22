6e74ffeef06101427e50dcb66d2932287b32afc8
Jean Joeris <joeris.peter@gmail.com>
2016-09-21 19:22:59 -0600

    Alphabetically sort items on category show

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,5 +2,6 @@ class CategoriesController < ApplicationController
 
   def show
     @category = Category.find_by_slug(params[:category_name])
+    @items = @category.items.order(title: :asc)
   end
 end
